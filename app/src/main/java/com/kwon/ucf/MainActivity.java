be127c3e939627633e2800f90fa2050a03edfda4
DAEWOO <rnlsoshk1@gmail.com>
2018-06-05 11:03:35 +0900

    first commit

---
+++ app/src/main/java/com/kwon/ucf/MainActivity.java
@@ -0,0 +1,87 @@
+package com.kwon.ucf;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.kwon.ucf.Fragments.Fragment1;
+import com.kwon.ucf.Fragments.Fragment2;
+import com.kwon.ucf.Fragments.Fragment3;
+
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener{
+
+    private ViewPager viewPager;
+    private BottomNavigationView navigation;
+
+    private Fragment1 fragment1 = new Fragment1();
+    private Fragment2 fragment2 = new Fragment2();
+    private Fragment3 fragment3 = new Fragment3();
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            switch (item.getItemId()) {
+//                case R.id.navigation_home:
+//                    return true;
+//                case R.id.navigation_dashboard:
+//                    return true;
+//                case R.id.navigation_notifications:
+//                    return true;
+//            }
+            viewPager.setCurrentItem(item.getOrder());
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.addOnPageChangeListener(this);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position){
+                    case 0:
+                        return fragment1;
+                    case 1:
+                        return fragment2;
+                    case 2:
+                        return fragment3;
+                }
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+        });
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        navigation.getMenu().getItem(position).setChecked(true);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+}
---
+++ app/src/main/java/com/kwon/ucf/Splash/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.kwon.ucf.Splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kwon.ucf.MainActivity;
+import com.kwon.ucf.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+    }
+}
